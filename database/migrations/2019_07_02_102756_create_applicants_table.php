b91cb767fef03c70150727f74793032c3c9c8560
saikiransaindla <sai.saindla@razorpay.com>
2019-07-02 17:49:01 +0530

    After user registration

---
+++ database/migrations/2019_07_02_102756_create_applicants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applicants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('mobile');
+            $table->string('address');
+            $table->string('gender');
+            $table->string('education');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('applicants');
+    }
+}
---
+++ database/migrations/2019_07_02_102812_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('about');
+            $table->string('address');
+            $table->string('contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('companies');
+    }
+}
